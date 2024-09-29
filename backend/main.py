a48664116cb5c76c004b774c1f1823327ad93c8f
bryanhce <ho.cheng.en.bryan@gmail.com>
2024-09-29 19:18:38 +0800

    expose folder routes for eg

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from routes.userRoutes import userRouter
 
 app = FastAPI()
 
+app.include_router(userRouter, prefix="/api/v1", tags=["users"])
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello World"}
+    return {"message": "Fast API is up and running"}
