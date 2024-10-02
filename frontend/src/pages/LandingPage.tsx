7210c5c6e496ad06425e656d0de24cd25099ce02
ARPspoofing <mohdreyaaz99@gmail.com>
2024-10-02 11:59:23 +0800

    Add review section

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -3,15 +3,19 @@ import AboutUs from "../components/AboutUs/AboutUs";
 import Steps from "../components/Steps/Steps";
 import Display from "../components/Display/Display";
 import Features from "../components/Features/Features";
+import Reviews from "../components/Reviews/Reviews";
 
 const LandingPage = () => {
-    return <div>
-        <Hero/>
-        <AboutUs/>
-        <Steps/>
-        <Display/>
-        <Features/>
-        </div>;
-  };
-  
-  export default LandingPage;
+  return (
+    <div>
+      <Hero />
+      <AboutUs />
+      <Steps />
+      <Display />
+      <Features />
+      <Reviews />
+    </div>
+  );
+};
+
+export default LandingPage;
