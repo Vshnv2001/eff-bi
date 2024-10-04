27fb04236d030f19bdd466e4973658caa175da42
jyztintan <justintanwk2001@gmail.com>
2024-10-04 21:51:57 +0800

    feat: implement create_organization

--- backend/effbi_api/views.py
+++ backend/effbi_api/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
 from .models import User, Organization
+from .serializer import UserSerializer, OrganizationSerializer
 
+
+@api_view(["GET"])
 def health_check(request):
     return JsonResponse({'message': 'Server is running successfully'}, status=200)
 
+
+@api_view(["POST"])
 def create_user(request):
     return JsonResponse({'message': 'User created successfully'}, status=201)
-        
+    
 def get_user(request, user_id):
     return JsonResponse({'message': 'User retrieved successfully'}, status=200)
 
@@ -23,8 +30,22 @@ def delete_user(request, user_id):
 def get_users_by_organization(request, org_id):
     return JsonResponse({'message': 'Users retrieved successfully'}, status=200)
 
+
+@api_view(["POST"])
 def create_organization(request):
-    return JsonResponse({'message': 'Organization created successfully'}, status=201)
+    try:
+        serializer = OrganizationSerializer(data=request.data)
+        if serializer.is_valid():
+            organization = serializer.save()
+            return JsonResponse(
+                {'message': 'Organization created successfully', 'organization': serializer.data},
+                status=status.HTTP_201_CREATED
+            )
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 def get_organization(request, org_id):
     return JsonResponse({'message': 'Organization retrieved successfully'}, status=200)
