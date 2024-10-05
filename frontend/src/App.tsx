95105c4c2f84534d284ed923c98a42a7375291bf
ARPspoofing <mohdreyaaz99@gmail.com>
2024-10-05 11:03:59 +0800

    Intgerate auth to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,52 @@
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
+import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
+import { SessionAuth } from "supertokens-auth-react/recipe/session";
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+//import LandingPage from "./pages/LandingPage";
+import {
+  PreBuiltUIList,
+  SuperTokensConfig,
+  ComponentWrapper,
+} from "./components/Authentication/Authentication";
+import * as reactRouterDom from "react-router-dom";
+
+SuperTokens.init(SuperTokensConfig);
+
+function App() {
+    return (
+        <SuperTokensWrapper>
+            <ComponentWrapper>
+                <div className="App app-container">
+                    <Router>
+                        <div className="fill">
+                            <Routes>
+                                {/* This shows the login UI on "/auth" route */}
+                                {getSuperTokensRoutesForReactRouterDom(reactRouterDom, PreBuiltUIList)}
+
+                                <Route
+                                    path="/"
+                                    element={
+                                        /* This protects the "/" route so that it shows
+                                    <Home /> only if the user is logged in.
+                                    Else it redirects the user to "/auth" */
+                                        <SessionAuth>
+                                            <Home />
+                                        </SessionAuth>
+                                    }
+                                />
+                            </Routes>
+                        </div>
+                    </Router>
+                </div>
+            </ComponentWrapper>
+        </SuperTokensWrapper>
+    );
+}
+
+export default App;
+
+
 /*
 import {
   Route,
@@ -51,6 +100,7 @@ const App = () => {
 export default App;
 */
 
+/*
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import LandingPage from "./pages/LandingPage";
@@ -72,3 +122,4 @@ const App = () => {
 };
 
 export default App;
+*/
