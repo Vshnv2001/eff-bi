1f3dfcea9bda531cd06fa175bc9f035a193f8885
jyztintan <justintanwk2001@gmail.com>
2024-10-08 22:18:36 +0800

    fix: change user_id to string in urls.py

--- backend/effbi_api/urls.py
+++ backend/effbi_api/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
     path("tenants/", views.TenantsAPI.as_view()),
     path("health/", views.health_check, name="health_check"),
     path("users/", views.create_user, name="create_user"),
-    path("users/<int:user_id>/", views.user_details, name="user_details"),
+    path("users/<str:user_id>/", views.user_details, name="user_details"),
     path("users/org/<int:org_id>/", views.get_users_by_organization, name="get_users_by_organization"),
     path("organizations/", views.create_organization, name="create_organization"),
     path("organizations/<int:org_id>/", views.organization_details, name="organization_details"),
     path("connection/", views.create_connection, name="create_connection"),
     path("query/", views.query_databases, name="query_databases"),
-    path("user-access-permissions/<int:user_id>", views.get_user_access_permissions, name="user_access_permissions")
+    path("user-access-permissions/<str:user_id>/", views.get_user_access_permissions, name="get_user_access_permissions")
 ]
