e7b446081fade0cc5288600efa3e3f2df3301aa9
ARPspoofing <mohdreyaaz99@gmail.com>
2024-10-12 20:41:38 +0800

    Add final landing page

--- frontend/src/components/AboutUs/AboutUs.tsx
+++ frontend/src/components/AboutUs/AboutUs.tsx
@@ -5,18 +5,21 @@ import ChartsGrid from "./ChartsGrid";
 const AboutUs = () => {
   return (
     <div className="w-full bg-black text-white">
-  <div className="w-full flex justify-center p-6">
-    <div className="w-1/2 h-1/2">
-      <ChartsGrid />
-    </div>
-  </div>
+      <div className="flex flex-col items-center justify-center p-6">
+        <h1 className="text-6xl text-white leading-[88px] max-md:text-4xl max-md:leading-[54px] mb-6 text-center">
+          Render Beautiful Charts
+        </h1>
+        <div className="w-1/2">
+          <ChartsGrid />
+        </div>
+      </div>
 
-  <div className="flex flex-wrap gap-6 items-start max-w-full bg-blend-normal min-h-[107px] w-full max-md:mt-10 p-6">
-    {Texts.map((text, index) => (
-      <TextCard key={index} {...text} />
-    ))}
-  </div>
-</div>
+      <div className="flex flex-wrap gap-6 items-start max-w-full bg-blend-normal min-h-[107px] w-full max-md:mt-10 p-6">
+        {Texts.map((text, index) => (
+          <TextCard key={index} {...text} />
+        ))}
+      </div>
+    </div>
   );
 };
 
