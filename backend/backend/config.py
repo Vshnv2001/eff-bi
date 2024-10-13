28732921710a63332db6d1b58e280938c252e1fe
Vshnv2001 <mvaishnav.sg@gmail.com>
2024-10-13 22:40:08 +0800

    Hopefully this works

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -3,15 +3,18 @@ from supertokens_python import (
     InputAppInfo,
     SupertokensConfig,
 )
+import dotenv
+import os
+dotenv.load_dotenv()
 
 # this is the location of the SuperTokens core.
 supertokens_config = SupertokensConfig(
-    connection_uri="https://try.supertokens.com")
+    connection_uri=os.getenv("SUPERTOKENS_CONNECTION_URI"))
 
 app_info = InputAppInfo(
     app_name="eff-bi",
-    api_domain="http://localhost:8000",
-    website_domain="http://localhost:3000",
+    api_domain=os.getenv("API_DOMAIN"),
+    website_domain=os.getenv("WEBSITE_DOMAIN"),
 )
 
 # recipeList contains all the modules that you want to
