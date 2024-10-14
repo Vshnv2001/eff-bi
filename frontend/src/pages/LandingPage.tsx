0eba56e7df6bb81cb33adf4bd003be1939253624
bryanhce <ho.cheng.en.bryan@gmail.com>
2024-10-14 23:58:29 +0800

    fix build

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,7 +2,6 @@ import Hero from "../components/Hero/Hero";
 import AboutUs from "../components/AboutUs/AboutUs";
 import Steps from "../components/Steps/Steps";
 import Display from "../components/Display/Display";
-import Features from "../components/Features/Features";
 import Reviews from "../components/Reviews/Reviews";
 import Chat from "../components/Footer/Chat";
 import ContactUs from "../components/Footer/ContactUs";
