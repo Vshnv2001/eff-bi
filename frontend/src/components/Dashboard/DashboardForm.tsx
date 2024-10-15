70200db150ecaae485a45aabbd85f85e1df1bd0d
ARPspoofing <mohdreyaaz99@gmail.com>
2024-10-15 20:21:14 +0800

    Add new dashboard styling

--- frontend/src/components/Dashboard/DashboardForm.tsx
+++ frontend/src/components/Dashboard/DashboardForm.tsx
@@ -8,7 +8,6 @@ import {
   Input,
   Textarea,
   IconButton,
-  Tooltip,
 } from "@material-tailwind/react";
 import { useState } from "react";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
