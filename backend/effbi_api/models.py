f531ba13716ea4db2b49089c19a9b98035e8a2ce
Vshnv2001 <mvaishnav.sg@gmail.com>
2024-10-17 20:07:39 +0800

    character

--- backend/effbi_api/models.py
+++ backend/effbi_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.CharField(max_length=255, primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
