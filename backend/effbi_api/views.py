3c53a994c8de268ca26ccc8587c45a8836ba6064
bryanhce <ho.cheng.en.bryan@gmail.com>
2024-10-24 19:19:32 +0800

    add logging

--- backend/effbi_api/views.py
+++ backend/effbi_api/views.py
@@ -10,11 +10,10 @@ from .models import Dashboard, Tile, User
 from rest_framework import status
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
-import concurrent.futures
-from .user_access_permissions.user_access_views import get_accessible_tables, add_permissions_to_user
 from .serializer import DashboardSerializer, TileSerializer
-from .helpers.table_preprocessing import get_database_schemas_and_tables, process_table
-import concurrent.futures
+import logging 
+
+logger = logging.getLogger(__name__)
 
 class SessionInfoAPI(APIView):
     @method_decorator(verify_session())
@@ -54,16 +53,16 @@ def health_check(request):
 def create_dashboard(request):
     try:
         user_id = request.supertokens.get_user_id()
-        print(user_id)
+        logger.error(user_id)
         user = get_object_or_404(User, id=user_id)
         org_id = user.organization.id
         request.data['organization'] = org_id
         request.data['created_by'] = user.first_name + " " + user.last_name
-        print(request.data)
+        logger.error(request.data)
         serializer = DashboardSerializer(data=request.data)
         if serializer.is_valid():
             dashboard = serializer.save()
-            print(dashboard)
+            logger.error(dashboard)
             return JsonResponse(
                 {'message': 'Dashboard created successfully',
                     'dashboard': serializer.data},
@@ -72,7 +71,7 @@ def create_dashboard(request):
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     except Exception as e:
-        print(e)
+        logger.error(e)
         return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
@@ -82,7 +81,7 @@ def create_dashboard(request):
 def get_dashboards(request: HttpRequest):
     session = request.supertokens
     user_id = session.get_user_id()
-    print(user_id)
+    logger.error(user_id)
     user = get_object_or_404(User, id=user_id)
     org_id = user.organization
     dashboards = Dashboard.objects.filter(organization=org_id)
@@ -92,12 +91,12 @@ def get_dashboards(request: HttpRequest):
 @api_view(["GET"])
 @verify_session()
 def get_dashboard_name(request: HttpRequest):
-    print("get_dashboard_name called")
+    logger.error("get_dashboard_name called")
     user_id = request.supertokens.get_user_id()
     user = get_object_or_404(User, id=user_id)
     org_id = user.organization.id
     dash_id = request.GET.get('dash_id', None)
-    print("dash_id: ", dash_id, "org_id: ", org_id)
+    logger.error("dash_id: ", dash_id, "org_id: ", org_id)
     dashboard = get_object_or_404(Dashboard, dash_id=dash_id, organization=org_id)
     return JsonResponse({'data': dashboard.title}, status=200)
 
@@ -105,16 +104,16 @@ def get_dashboard_name(request: HttpRequest):
 @api_view(["GET"])
 @verify_session()
 def get_dashboard_tiles(request: HttpRequest):
-    print("get tiles request called")
+    logger.error("get tiles request called")
     user_id = request.supertokens.get_user_id()
     dash_id = request.GET.get('dash_id', None)
     user = get_object_or_404(User, id=user_id)
     org_id = user.organization.id
-    print("dash_id: ", dash_id)
+    logger.error("dash_id: ", dash_id)
     tiles = Tile.objects.filter(dash_id=dash_id, organization=org_id)
-    print("filtered tiles: ", tiles)
+    logger.error("filtered tiles: ", tiles)
     serializer = TileSerializer(tiles, many=True)
-    print("data", serializer.data)
+    logger.error("data", serializer.data)
     return JsonResponse({'data': serializer.data}, status=200)
 
 
@@ -123,7 +122,7 @@ def get_dashboard_tiles(request: HttpRequest):
 def create_dashboard_tile(request: HttpRequest):
     try:
         dash_id = request.data.get('dash_id', None)
-        print(dash_id)
+        logger.error(dash_id)
         if not dash_id:
             return JsonResponse({'error': "Dash_id is required"}, status=status.HTTP_400_BAD_REQUEST)
         user_id = request.supertokens.get_user_id()
@@ -131,21 +130,21 @@ def create_dashboard_tile(request: HttpRequest):
         org_id = user.organization.id
         db_uri = user.organization.database_uri
         response : State = response_pipeline(request.data.get('description'), db_uri, org_id, user_id)
-        print("Pipeline complete")
+        logger.error("Pipeline complete")
         if response.error != "":
-            print(response.error)
+            logger.error(response.error)
             return JsonResponse({'error': response.error}, status=status.HTTP_400_BAD_REQUEST)
         request.data['organization'] = org_id
         request.data['sql_query'] = response.sql_query
         request.data['component'] = response.visualization.get('visualization', '')
         request.data['tile_props'] = response.formatted_data
-        print(request.data)
+        logger.error(request.data)
         serializer = TileSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse({'data': serializer.data}, status=201)
-        print(serializer.errors)
+        logger.error(serializer.errors)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
-        print(e)
+        logger.error(e)
         return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
