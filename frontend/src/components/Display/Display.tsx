97193bfecca400bac652b69a34084939368f47e8
ARPspoofing <mohdreyaaz99@gmail.com>
2024-10-27 17:59:41 +0800

    Add consistent colors in landing page

--- frontend/src/components/Display/Display.tsx
+++ frontend/src/components/Display/Display.tsx
@@ -3,7 +3,7 @@ import ImageGrid from "./ImageGrid";
 
 const Display: React.FC = () => {
   return (
-    <main className="flex overflow-hidden flex-col p-16 bg-white max-md:px-5">
+    <main className="flex overflow-hidden flex-col p-16 bg-[#f0f2fe] max-md:px-5">
       <Header />
       <ImageGrid />
     </main>
