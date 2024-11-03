2b4a2c8f5e2c62d89d7f43984372f165759285f4
ARPspoofing <mohdreyaaz99@gmail.com>
2024-11-03 14:39:25 +0800

    Remove radar chart polar and pyramid bar chart

--- frontend/src/components/Dashboard/ComponentMapping.tsx
+++ frontend/src/components/Dashboard/ComponentMapping.tsx
@@ -2,10 +2,8 @@ import { BarChartTemplate } from "../ChartTemplates/BarChartTemplates/BarChartTe
 import { HorizontalBarChartTemplate } from "../ChartTemplates/BarChartTemplates/HorizontalBarChartTemplate";
 import { DonutChartTemplate } from "../ChartTemplates/PieChartTemplates/DonutChartTemplate";
 import { AreaChartTemplate } from "../ChartTemplates/AreaChartTemplates/AreaChartTemplate";
-import { PyramidBarChartTemplate } from "../ChartTemplates/BarChartTemplates/PyramidBarChartTemplate";
 import PieChartTemplate from "../ChartTemplates/PieChartTemplates/PieChartTemplate";
 import RadarChartTemplate from "../ChartTemplates/RadarChartTemplates/RadarChartTemplate";
-import RadarChartPolarTemplate from "../ChartTemplates/RadarChartTemplates/RadarChartPolarTemplate";
 import ScatterChartTemplate from "../ChartTemplates/LineChartTemplates/ScatterChartTemplate";
 import CandlestickTemplate from "../ChartTemplates/BoxPlotTemplates/CandleStickTemplate";
 import BoxPlotTemplate from "../ChartTemplates/BoxPlotTemplates/BoxPlotTemplate";
@@ -18,10 +16,8 @@ export const componentMapping = {
   'HorizontalBarChartTemplate': HorizontalBarChartTemplate,
   'DonutChartTemplate': DonutChartTemplate,
   'AreaChartTemplate': AreaChartTemplate,
-  'PyramidBarChartTemplate': PyramidBarChartTemplate,
   'PieChartTemplate': PieChartTemplate,
   'RadarChartTemplate': RadarChartTemplate,
-  'RadarChartPolarTemplate': RadarChartPolarTemplate,
   'ScatterChartTemplate': ScatterChartTemplate,
   'CandlestickTemplate': CandlestickTemplate,
   'BoxPlotTemplate': BoxPlotTemplate,
@@ -35,10 +31,8 @@ export const componentNames = {
   'Horizontal Bar Chart': 'HorizontalBarChartTemplate',
   'Donut Chart': 'DonutChartTemplate',
   'Area Chart': 'AreaChartTemplate',
-  'Pyramid Bar Chart': 'PyramidBarChartTemplate',
   'Pie Chart': 'PieChartTemplate',
   'Radar Chart': 'RadarChartTemplate',
-  'Radar Chart Polar': 'RadarChartPolarTemplate',
   'Scatter Chart': 'ScatterChartTemplate',
   'Candlestick Chart': 'CandlestickTemplate',
   'Box Plot Chart': 'BoxPlotTemplate',
