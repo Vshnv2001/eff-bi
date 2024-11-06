49e0d1b4bf7790af68007406932bf16b0251e14f
bryanhce <ho.cheng.en.bryan@gmail.com>
2024-11-06 21:29:05 +0800

    change no table text

--- frontend/src/pages/NewDataPage/ViewDataPage.tsx
+++ frontend/src/pages/NewDataPage/ViewDataPage.tsx
@@ -1,17 +1,17 @@
 import { useState, useEffect } from "react";
-import {Button, Card, Spinner, Tooltip} from "@material-tailwind/react";
+import { Button, Card, Spinner, Tooltip } from "@material-tailwind/react";
 import axios from "axios";
 import { BACKEND_API_URL } from "../../config/index";
 import { useSessionContext } from "supertokens-auth-react/recipe/session";
 import LayersIcon from "@mui/icons-material/Layers";
-import {DashboardLayout} from "@toolpad/core/DashboardLayout";
-import {AppProvider} from "@toolpad/core/AppProvider";
-import {useDemoRouter} from "@toolpad/core/internal";
-import {createTheme} from "@mui/material/styles";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { useDemoRouter } from "@toolpad/core/internal";
+import { createTheme } from "@mui/material/styles";
 import TablePage from "./TablePage.tsx";
-import {toast} from "react-toastify";
-import {useAuth} from "../../components/Authentication/AuthenticationContext.tsx";
-import {Typography} from "@mui/material";
+import { toast } from "react-toastify";
+import { useAuth } from "../../components/Authentication/AuthenticationContext.tsx";
+import { Typography } from "@mui/material";
 
 interface Table {
   table_name: string;
@@ -20,13 +20,11 @@ interface Table {
   rows: string[][];
 }
 
-
 const demoTheme = createTheme({
   cssVariables: { colorSchemeSelector: "data-toolpad-color-scheme" },
   colorSchemes: { light: true },
   breakpoints: { values: { xs: 0, sm: 600, md: 600, lg: 1200, xl: 1536 } },
 });
-
 
 export default function ViewDataPage() {
   const [tables, setTables] = useState<Table[]>([]);
@@ -45,7 +43,7 @@ export default function ViewDataPage() {
       title: table.table_name,
       icon: <LayersIcon />,
     })),
-    { kind: "divider" as const},
+    { kind: "divider" as const },
   ];
   const handleRefresh = async () => {
     const reqBody = {
@@ -129,9 +127,9 @@ export default function ViewDataPage() {
     pathname = pathname.replace("/", "");
     const table = tables.find((table) => table.table_name === pathname);
     if (!table) {
-      return <TablePage table={tables[0]}/>
+      return <TablePage table={tables[0]} />;
     }
-    return <TablePage table={table}/>
+    return <TablePage table={table} />;
   }
 
   useEffect(() => {
@@ -156,7 +154,7 @@ export default function ViewDataPage() {
   if (loading) {
     return (
       <div className="fixed inset-0 flex items-center justify-center bg-gray-300 bg-opacity-50 z-50">
-        <Spinner className="h-10 w-10"/>
+        <Spinner className="h-10 w-10" />
       </div>
     );
   }
@@ -164,8 +162,8 @@ export default function ViewDataPage() {
   if (tables.length == 0) {
     return (
       <div className="flex items-center justify-center min-h-screen bg p-10">
-        <Card className="w-full max-w-4xl p-10 rounded-xl" color="blue-gray">
-          <div className="text-lg text-center text-black">
+        <Card className="w-full max-w-4xl p-10 rounded-xl" color="blue">
+          <div className="text-2xl text-center text-black">
             You do not have access to any tables, request for permissions with
             your administrator.
           </div>
@@ -174,28 +172,23 @@ export default function ViewDataPage() {
     );
   }
   return (
-    <AppProvider
-      navigation={NAVIGATION}
-      router={router}
-      theme={demoTheme}
-    >
+    <AppProvider navigation={NAVIGATION} router={router} theme={demoTheme}>
       {refreshLoading && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <Spinner className="h-10 w-10" />
         </div>
-        )}
+      )}
 
       <DashboardLayout
-          slots={{
-            sidebarFooter: SidebarFooter,
-          }}
-          sx={{height: "calc(100vh - 60px)", overflow:"auto"}}
+        slots={{
+          sidebarFooter: SidebarFooter,
+        }}
+        sx={{ height: "calc(100vh - 60px)", overflow: "auto" }}
       >
-        <div style={{maxWidth: 'calc(100vw - 320px)'}}>
-          <DashboardPageContent pathname={router.pathname}/>
+        <div style={{ maxWidth: "calc(100vw - 320px)" }}>
+          <DashboardPageContent pathname={router.pathname} />
         </div>
-
       </DashboardLayout>
     </AppProvider>
   );
-};
+}
