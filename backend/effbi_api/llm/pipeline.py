84db6c40ed7c9a89ee921ead4b2fdc2f9561d13c
ARPspoofing <mohdreyaaz99@gmail.com>
2024-11-08 00:02:11 +0800

    Add stream

--- backend/effbi_api/llm/pipeline.py
+++ backend/effbi_api/llm/pipeline.py
@@ -7,32 +7,35 @@ from .DataFormatter import DataFormatter
 from .PrunerAgent import PrunerAgent
 from .State import State
 from ..helpers.access_check import get_accessible_table_names
-import logging 
+import logging
+import time
 
 logger = logging.getLogger(__name__)
+
 
 def response_pipeline(user_query: str, db_uri: str, organization_id: int, user_id: int):
     state = State()
     state.question = user_query
-     
+
     accessible_table_names = get_accessible_table_names(user_id)
     state.accessible_table_names = accessible_table_names
     logger.info("ACCESSIBLE TABLE NAMES: " + str(accessible_table_names))
-    
-    state.total_tables = [table.table_name for table in OrgTables.objects.filter(organization_id=organization_id)]
-    
+
+    state.total_tables = [table.table_name for table in OrgTables.objects.filter(
+        organization_id=organization_id)]
+
     # Get the database schema
     db_manager = DatabaseManager(db_uri, organization_id)
     database_schema = db_manager.get_schema(accessible_table_names)
-    
+
     state.database_schema = database_schema
-        
+
     # Prune the database schema to identify relevant tables and columns
     pruner = PrunerAgent()
     parsed_question = pruner.prune(state)
-    
+
     state.parsed_question = parsed_question
-    
+
     # logger.info("PARSED QUESTION: " + str(state.parsed_question))
 
     if not state.parsed_question['is_relevant']:
@@ -48,18 +51,17 @@ def response_pipeline(user_query: str, db_uri: str, organization_id: int, user_i
     # Pass the pruned schema to the SQL agent to generate a SQL query
     sql_agent = SQLAgent(db_manager)
     sql_query = sql_agent.generate_sql(state)
-    
+
     logger.info("SQL QUERY: " + str(sql_query))
 
     if sql_query["sql_query"] == "NOT_RELEVANT":
         state.error = "Your question is not relevant to the data in your database. Please modify your question or update your database."
         return state
-    
-    
+
     state.sql_query = sql_query.get('sql_query', '')
-    
+
     sql_validator = SQLValidator()
-    
+
     # Execute the SQL query and get the results
     try:
         results = db_manager.execute_query(state, sql_validator, 0)
@@ -68,8 +70,11 @@ def response_pipeline(user_query: str, db_uri: str, organization_id: int, user_i
         state.error = "We were unable to generate a valid SQL query. Please rephrase your question."
         return state
     
+    yield {"sql_query": state.sql_query}
+
     state.results = results
-    
+
+    start_time = time.time()
     # Format the results
     formatter = DataFormatter(state)
     visualization_choice = formatter.choose_visualization()
@@ -81,29 +86,35 @@ def response_pipeline(user_query: str, db_uri: str, organization_id: int, user_i
         logger.info("Error formatting data for visualization: " + str(e))
         state.error = "We are unable to visualize the data. Please try a different question or provide more information."
         return state
-    
+
     logger.info("FORMATTED DATA: " + str(formatted_data))
 
-    return state
+    end_time = time.time()
+    print(f"executed_in_later: {end_time - start_time:.4f} seconds")
+
+    yield {"state": state}
+
+    #return state
+
 
 def refresh_dashboard_tile_pipeline(state: State, db_uri: str, organization_id: int):
     db_manager = DatabaseManager(db_uri, organization_id)
 
     sql_validator = SQLValidator()
-    
+
     results = db_manager.execute_query(state, sql_validator, 0)
     state.results = results
     logger.info(f"RESULTS: {results}")
     formatter = DataFormatter(state)
     try:
         formatted_data = formatter.format_data_for_visualization()
-        
+
     except Exception as e:
         logger.info("Error formatting data for visualization: ", e)
         state.error = "We are unable to visualize the data. Please try a different question or provide more information."
         raise Exception(state.error)
-    
+
     logger.info(f"FORMATTED DATA: {formatted_data}")
-    
+
     state.formatted_data = formatted_data
     return state
