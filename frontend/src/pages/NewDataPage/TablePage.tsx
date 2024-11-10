5af948a2b21b4785d0c4e73d945e9b5ca44ab46f
jyztintan <justintanwk2001@gmail.com>
2024-11-10 16:07:54 +0800

    feat: add warning for sample rows

--- frontend/src/pages/NewDataPage/TablePage.tsx
+++ frontend/src/pages/NewDataPage/TablePage.tsx
@@ -3,6 +3,7 @@ import DataTable from "../../components/DataTable/DataTable";
 import {Alert} from "@mui/material";
 import AutoAwesomeOutlinedIcon from '@mui/icons-material/AutoAwesomeOutlined';
 import Stack from "@mui/material/Stack";
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 
 interface Table {
     index: number;
@@ -53,6 +54,11 @@ const TablePage: React.FC<TablePageProps> = ({table}: {table: Table}) => {
                 }))}
                 data={table.rows}
             />
+            <Alert variant="outlined" icon={<PriorityHighIcon/>} severity="warning" className="m-4" sx={{
+                    width : "100%",
+                }}>
+                Only the first 100 rows are displayed.
+            </Alert>
         </div>
     );
 };
